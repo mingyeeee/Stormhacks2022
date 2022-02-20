6314e5d93aa9574cb606575198fb81d5e7998b7f
Brevin Baskaran <brevin.baskaran@outlook.com>
2022-02-19 23:14:47 -0500

    fixed format

--- analysis.py
+++ analysis.py
@@ -25,12 +25,10 @@ text1 = '''
   <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;background:#ffffff;">
     <tr>
       <td align="center" style="padding:0;">
-        <table role="presentation" style="width:402px;border-collapse:collapse;border:0px solid #cccccc;border-spacing:0;text-align:left;">
+        <table role="presentation" style="width:602px;border-collapse:collapse;border:1px solid #cccccc;border-spacing:0;text-align:left;">
           <tr>
-            <td align="center" style="padding:40px 0 30px 0;background:#70bbd9;">
-
- 
-              <img src="thumbnail.jpg" alt="" width="300" style="height:auto;display:block;" />
+            <td align="center" style="padding:40px 0 30px 0;background:#47a2a4;">
+              <img src="thumbnail.jpg" alt="" width="200"  style="height:auto;display:block;" />
             </td>
           </tr>
           <tr>
@@ -38,39 +36,57 @@ text1 = '''
               <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;">
                 <tr>
                   <td style="padding:0 0 36px 0;color:#153643;">
-                    <h1 style="font-size:11px;margin:0 0 12px 0;font-family:Arial,sans-serif;font-style: italic;">MAKING EDUCATION UNDERSTANBLE IN THE DIGITAL AGE</h1>
-                    <p style="margin:0 0 20px 0;font-size:34px;line-height:24px;font-family:Arial,sans-serif; color = blue">Transcribed Notes</p>
+                    <h1 style="font-size:24px;margin:0 0 20px 0;font-family:Arial,sans-serif;">Lecture #__</h1>
+                    <p style="margin:0 0 12px 0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;">SOME INTRO TEXT</p>
+                    <p style="margin:0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;"><a href="GITHUB REPO LINK" style="color:#ee4c50;text-decoration:underline;">Notes Generated using NoteAI</a></p>
                   </td>
                 </tr>
 
-      
 
                 <tr>
                   <td style="padding:0;">
                     <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;">
-                      <tr>
-                        <td style="width:20px;padding:0;font-size:0;line-height:100;">&nbsp;</td>
-                        <td style="width:260px;padding:0;vertical-align:top;color:#153643;">
-                          <p style="margin:0 0 25px 0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;">
+                      
+                  
+
+
+                      
+
+                    
 '''                        
 
-txt2 = '''            
-width = "50%"
- 
-</center>
-                          </p>
-                          
-                        
+end = '''
+</table>
+                  </td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+          <tr>
+            <td style="padding:30px;background:#ee4c50;">
+              <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;font-size:9px;font-family:Arial,sans-serif;">
+                <tr>
+                  <td style="padding:0;width:50%;" align="left">
+                    <p style="margin:0;font-size:14px;line-height:16px;font-family:Arial,sans-serif;color:#ffffff;">
+                      &reg; Someone, Somewhere 2021<br/><a href="http://www.example.com" style="color:#ffffff;text-decoration:underline;">Unsubscribe</a>
+                    </p>
+                  </td>
+                  <td style="padding:0;width:50%;" align="right">
+                    <table role="presentation" style="border-collapse:collapse;border:0;border-spacing:0;">
+                      <tr>
+                        <td style="padding:0 0 0 10px;width:38px;">
+                          <a href="http://www.twitter.com/" style="color:#ffffff;"><img src="https://assets.codepen.io/210284/tw_1.png" alt="Twitter" width="38" style="height:auto;display:block;border:0;" /></a>
+                        </td>
+                        <td style="padding:0 0 0 10px;width:38px;">
+                          <a href="http://www.facebook.com/" style="color:#ffffff;"><img src="https://assets.codepen.io/210284/fb_1.png" alt="Facebook" width="38" style="height:auto;display:block;border:0;" /></a>
                         </td>
                       </tr>
-
                     </table>
                   </td>
                 </tr>
               </table>
             </td>
           </tr>
-          
         </table>
       </td>
     </tr>
@@ -81,8 +97,17 @@ width = "50%"
 '''
 
 for file in glob.glob("*.png"):
+
    
-    text1 = text1 + "<center><img src=\"" + file + "\"" + txt2
- 
+    text1 = text1 + '''<tr>
+                        <td style="width:260px;padding:0;vertical-align:top;color:#153643;">
+                          <p style="margin:0 0 25px 0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;"><img src="''' + file +'''" alt="" width="260" style="height:auto;display:block;" /></p>
+                          <p style="margin:0 0 12px 0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;">OUR TEXT GOES HERE</p>
+                          <p style="margin:0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;"><a href="" style="color:#ee4c50;text-decoration:underline;">LINKS CAN GO HERE</a></p>
+                        </td>
+                       
+                      </tr>'''
+text1 = text1 + end
+
 with open("index.html", "w") as outputfile:
 	outputfile.write(text1)
