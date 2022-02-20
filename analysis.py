7f2986299d6426d90e5a2a03c620daf1a62b61e6
Brevin Baskaran <brevin.baskaran@outlook.com>
2022-02-19 21:50:19 -0500

    finished most of html and design

--- analysis.py
+++ analysis.py
@@ -1,13 +1,89 @@
-
 import glob
  
-html = ""
+text1 = '''
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <meta name="x-apple-disable-message-reformatting">
+  <title></title>
+  <!--[if mso]>
+  <noscript>
+    <xml>
+      <o:OfficeDocumentSettings>
+        <o:PixelsPerInch>96</o:PixelsPerInch>
+      </o:OfficeDocumentSettings>
+    </xml>
+  </noscript>
+  <![endif]-->
+  <style>
+    table, td, div, h1, p {font-family: Arial, sans-serif;}
+  </style>
+</head>
+<body style="margin:0;padding:0;">
+  <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;background:#ffffff;">
+    <tr>
+      <td align="center" style="padding:0;">
+        <table role="presentation" style="width:402px;border-collapse:collapse;border:0px solid #cccccc;border-spacing:0;text-align:left;">
+          <tr>
+            <td align="center" style="padding:40px 0 30px 0;background:#70bbd9;">
 
-for file in glob.glob("*.png"):
+ 
+              <img src="logo.jpg" alt="" width="300" style="height:auto;display:block;" />
+            </td>
+          </tr>
+          <tr>
+            <td style="padding:36px 30px 42px 30px;">
+              <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;">
+                <tr>
+                  <td style="padding:0 0 36px 0;color:#153643;">
+                    <h1 style="font-size:11px;margin:0 0 12px 0;font-family:Arial,sans-serif;font-style: italic;">MAKING EDUCATION UNDERSTANBLE IN THE DIGITAL AGE</h1>
+                    <p style="margin:0 0 20px 0;font-size:34px;line-height:24px;font-family:Arial,sans-serif; color = blue">Transcribed Notes</p>
+                  </td>
+                </tr>
+
+      
+
+                <tr>
+                  <td style="padding:0;">
+                    <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;">
+                      <tr>
+                        <td style="width:20px;padding:0;font-size:0;line-height:100;">&nbsp;</td>
+                        <td style="width:260px;padding:0;vertical-align:top;color:#153643;">
+                          <p style="margin:0 0 25px 0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;">
+'''                        
+
+txt2 = '''            
+width = "50%"
+ 
+</center>
+                          </p>
+                          
+                        
+                        </td>
+                      </tr>
+
+                    </table>
+                  </td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+          
+        </table>
+      </td>
+    </tr>
+  </table>
+</body>
+</html>
+
+'''
+
+for file in glob.glob("*.jpg"):
    
-	##html += f'<img src ={file}/>'
-    html += "<img src=\"" + file + "\"><br>"
+    text1 = text1 + "<center><img src=\"" + file + "\"" + txt2
  
 with open("index.html", "w") as outputfile:
-	outputfile.write(html)
+	outputfile.write(text1)
 
